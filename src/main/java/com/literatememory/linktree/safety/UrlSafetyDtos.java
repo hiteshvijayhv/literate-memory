ff04df251eaea2a6d53071ed59fab41888f31391
Hitesh <46933160+hiteshvijayhv@users.noreply.github.com>
2026-02-26 08:27:53 +0000

    Delete irrelevant files

--- src/main/java/com/literatememory/linktree/safety/UrlSafetyDtos.java
+++
@@ -1,8 +0,0 @@
-package com.literatememory.linktree.safety;
-
-import jakarta.validation.constraints.NotBlank;
-
-public class UrlSafetyDtos {
-    public record UrlCheckRequest(@NotBlank String url) {}
-    public record UrlCheckResponse(boolean allowed, String reason) {}
-}
